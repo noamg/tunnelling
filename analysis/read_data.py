c6b073a02c9391c8a63cb58271ac55883ab87c54
noam <gavishnoam@gmail.com>
2016-10-24 15:17:22 +0300

    reading data, prepare to create figure

--- analysis/read_data.py
+++ analysis/read_data.py
@@ -5,6 +5,8 @@ Created on Tue Oct 11 10:55:09 2016
 @author: noam
 """
 
+import os
+
 import numpy as np
 import scipy as sp
 from scipy import io
@@ -17,6 +19,21 @@ f_I_H_keithley = '/home/noam/studies/physics/amirim_hadar/tunnelling.git/measure
 f_I_H_magnet = '/home/noam/studies/physics/amirim_hadar/tunnelling.git/measurements/JCUL-0057.mat'
 f_I_record = ''
 
+figs_dir = '/home/noam/studies/physics/amirim_hadar/tunnelling.git/figs/'
+data_dir = '/home/noam/studies/physics/amirim_hadar/tunnelling.git/measurements/'
+data_prefix = 'JCUL-'
+data_format = '.mat'
+fig_format = '.svg'
+print(os.path.join(figs_dir + '0000' + fig_format))
+print(os.path.join(data_dir + 'JCUL-' + '0103' + '.mat'))
+assert os.path.join(data_dir + data_prefix + '0103' + data_format) == '/home/noam/studies/physics/amirim_hadar/tunnelling.git/measurements/JCUL-0103.mat'
+
+
+N_I_V_H = '0103'
+N_I_V = '0009'
+N_I_H_keithley = '0017'
+N_I_H_magnet = '0057'
+N_I_record = ''
 
 def read_mesdata(f):
     out = sp.io.loadmat(f)
@@ -89,12 +106,12 @@ def test_read_I_H():
     _ = plt.figure()
     _ = plt.plot(H, Idc, '.')
 
+if False:
+    test_read_mesdata()
+    test_read_I_V()
+    test_read_I_H()
 
-test_read_mesdata()
-test_read_I_V()
-test_read_I_H()
-
-def process_I_V(f, amp, Vdc_div=100, SF=11, axes=None):
+def process_I_V(f, amp, Vdc_div=100, SF=11, axes=None, label=''):
     if axes is None:
         fig, axes = plt.subplots(2, 1, sharex=True)
     mask = np.ones(SF) * 1.0 / SF
@@ -107,30 +124,63 @@ def process_I_V(f, amp, Vdc_div=100, SF=11, axes=None):
     dIdVdc[-N:] = dIdVdc[-N:] * np.arange(N, SF-1) / N
     
     ax1, ax2 = axes
-    ax1.plot(Vdc, Idc, '.')
-    ax1.set_xlabel('V[volt]')
-    ax1.set_ylabel('I[amp]')
-    ax2.plot(Vdc[1:,], dIdVdc, '.')
-    ax1.set_xlabel('V[volt]')
-    ax1.set_ylabel('G[siemens]')
+    _ = ax1.plot(Vdc * 1e3, Idc * 1e9, '.', label=label)
+    ax1.set_xlabel('V[mV]')
+    ax1.set_ylabel('I[nA]')
+    _ = ax2.plot(Vdc[1:,] * 1e3, dIdVdc * 1e6, '.', label=label)
+    ax2.set_xlabel('V[mV]')
+    ax2.set_ylabel(r'G[$\mu$S]')
 
-def process_I_H(f, amp, I_min, dV, ax=None):
+def process_I_H(f, amp, I_min, dV, ax=None, label=''):
     if ax is None:
         fig = plt.figure()
         ax = plt.subplot(111)
     H, Idc = read_I_H(f, amp)
     dIdV_zero_bias = (Idc - I_min) / dV
-    ax.plot(H, dIdV_zero_bias, '.')
+    _ = ax.plot(H, dIdV_zero_bias * 1e9, '.', label=label)
     ax.set_xlabel('H[T]')
-    ax.set_ylabel('G[siemens]')
+    ax.set_ylabel('G[nS]')
+    
     
      
+if False:
+    fig, axes = plt.subplots(2, 1, sharex=True)     
+    process_I_V(os.path.join(data_dir + data_prefix + N_I_V + data_format), amp=1e9, axes=axes, label='label')
+    axes[0].legend(loc='best')
+    axes[1].legend(loc='best')
+    fig.savefig(os.path.join(figs_dir + N_I_V + fig_format))
+    
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    process_I_H(os.path.join(data_dir + data_prefix + N_I_H_keithley + data_format), 1e9, 0, 100e-6, ax, label='label')
+    ax.legend(loc='best')
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    process_I_H(os.path.join(data_dir + data_prefix + N_I_H_magnet + data_format), 1e9, 0, 100e-6, ax, label='label')
+    ax.legend(loc='best')
 
-fig, axes = plt.subplots(2, 1, sharex=True)     
-process_I_V(f_I_V, amp=1e9, axes=axes)
-fig = plt.figure()
-ax = plt.subplot(111)
-process_I_H(f_I_H_keithley, 1e9, 0, 100e-6, ax)
+#%% dIdV low bias few T
+fig, axes = plt.subplots(2, 1, sharex=True)
+Ns = []
+amps = []
+labels = []
+assert len(Ns) == len(amps)
+for i in range(len(Ns)):
+    N = Ns[i]
+    amp = amps[i]
+    label = labels[i]
+    process_I_V(os.path.join(data_dir + data_prefix + N + data_format), amp=amp, axes=axes, label=label)
+axes[0].legend(loc='best')
+axes[1].legend(loc='best')
+fig.savefig(os.path.join(figs_dir + 'conductance_low_bias_few_T' + fig_format))
+
+
+
+#%%
+
+
+
+plt.show()
 
 if False:
     H = data[:,0]
