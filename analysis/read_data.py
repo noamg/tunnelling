0dd291ecb77c0818f2ef80e21fe9d55d6a72c2c0
noam <gavishnoam@gmail.com>
2016-10-25 17:27:19 +0300

    more ZBC, field cooling, records bistability

--- analysis/read_data.py
+++ analysis/read_data.py
@@ -82,7 +82,7 @@ def read_I_H(f, amp, tesla_ampere_ratio=1207.7e-4):
 def read_record(f, amp):
     sweeped, measured, data, measured_time, T_range = read_mesdata(f)
     assert sweeped[0] == 'time'
-    time = data[0]
+    time = data[:, 0]
     if 'dmm1.dcv' in measured[1]:
         Idc = data[:, len(sweeped) + 1] / amp
         
@@ -147,8 +147,18 @@ def process_I_H(f, amp, I_min, dV, ax=None, label=''):
     _ = ax.plot(H, dIdV_zero_bias * 1e9, '.', label=label)
     ax.set_xlabel('H[T]')
     ax.set_ylabel('G[nS]')
-    
-    
+
+def process_record(f, amp, I_min, dV, ax=None, label=''):
+    if ax is None:
+        fig = plt.figure()
+        ax = plt.subplot(111)
+    time, Idc = read_record(f, amp)
+    dIdV_zero_bias = (Idc - I_min) / dV
+    _ = ax.plot(time, dIdV_zero_bias * 1e9, '.', label=label)
+    ax.set_xlabel('time[s]')
+    ax.set_ylabel('G[nS]')
+
+#%%    
      
 if False:
     fig, axes = plt.subplots(2, 1, sharex=True)     
@@ -187,7 +197,7 @@ fig.savefig(os.path.join(figs_dir + 'conductance_low_bias_few_T' + fig_format))
 # maybe should shift the minimum to v = 0
 fig, axes = plt.subplots(2, 1, sharex=True)
 Ns = ['0001', '0002', '0005', '0010', '0012', '0013', '0016']
-amps = [1e9, 1e9, 1e9, 1e9, 1e9, 1e9, 1e9]
+amps = [1e9, 1e9, 1e9, 1e9, 1e9, 1e9, 1e10]
 labels = ['4K', '4K', '2.2K', '0.3K', '0.3K', '0.3K', '0.3K']
 assert len(Ns) == len(amps)
 for i in range(len(Ns)):
@@ -226,8 +236,8 @@ fig.savefig(os.path.join(figs_dir + 'conductance_high_bias_few_T' + fig_format))
 fig = plt.figure()
 ax = plt.subplot(111)
 Ns = ['0017', '0019']
-amps = [1e9, 1e9,]
-labels = ['', '']
+amps = [1e10, 1e10,]
+labels = ['up', 'down']
 I_mins = [0, 0,]
 dVs = np.array([100e-6, 100e-6])
 assert len(Ns) == len(amps)
@@ -237,7 +247,7 @@ for i in range(len(Ns)):
     label = labels[i]
     I_min = I_mins[i]
     dV = dVs[i]
-    process_I_H(os.path.join(data_dir + data_prefix + N + data_format), amp=amp, I_min=I_min, dV=dV, ax=ax, label='label')
+    process_I_H(os.path.join(data_dir + data_prefix + N + data_format), amp=amp, I_min=I_min, dV=dV, ax=ax, label=label)
 ax.legend(loc='best')
 fig.savefig(os.path.join(figs_dir + 'ZBC_low_H' + fig_format))
 
@@ -245,7 +255,7 @@ fig.savefig(os.path.join(figs_dir + 'ZBC_low_H' + fig_format))
 #%% conductance_before_after_ZBC
 fig, axes = plt.subplots(2, 1, sharex=True)
 Ns = ['0016', '0018', '0020']
-amps = [1e9, 1e9, 1e9]
+amps = [1e10, 1e10, 1e10]
 labels = ['H=0 before', 'H=0.12[T]', 'H=0 after']
 assert len(Ns) == len(amps)
 for i in range(len(Ns)):
@@ -269,8 +279,8 @@ for N in Ns:
 #%% field cooling dIdV
         
 fig, axes = plt.subplots(2, 1, sharex=True)
-Ns = ['0016', '0051',]
-amps = [1e9, 1e8]
+Ns = ['0016', '0051',] # can add 55, 56, 58, 62
+amps = [1e10, 1e9]
 labels = ['zero cooling', 'H=0.12[T] cooling']
 assert len(Ns) == len(amps)
 for i in range(len(Ns)):
@@ -280,8 +290,72 @@ for i in range(len(Ns)):
     process_I_V(os.path.join(data_dir + data_prefix + N + data_format), amp=amp, axes=axes, label=label)
 axes[0].legend(loc='best')
 fig.savefig(os.path.join(figs_dir + 'field_cooling_conductance' + fig_format))
-#%%
+#%% field cooling ZBC
 
+fig = plt.figure()
+ax = plt.subplot(111)
+Ns = ['0017', '0019', '0053']
+amps = [1e10, 1e10, 1e9]
+labels = ['ZC up', 'ZC down', 'FC']
+I_mins = [0, 0, 0,]
+dVs = np.array([100e-6, 100e-6, 100e-6, 100e-6])
+assert len(Ns) == len(amps)
+for i in range(len(Ns)):
+    N = Ns[i]
+    amp = amps[i]
+    label = labels[i]
+    I_min = I_mins[i]
+    dV = dVs[i]
+    process_I_H(os.path.join(data_dir + data_prefix + N + data_format), amp=amp, I_min=I_min, dV=dV, ax=ax, label=label)
+ax.legend(loc='best')
+fig.savefig(os.path.join(figs_dir + 'ZBC_field_cooling' + fig_format))
+
+
+#%%field cooling ZBC 1T
+fig = plt.figure()
+ax = plt.subplot(111)
+Ns = ['0053', '0057', '0060']
+amps = [1e9, 1e9, 1e9]
+labels = ['FC up 0.12T', 'FC up 1T', 'FC down']
+I_mins = [0, 0, 0,]
+dVs = np.array([100e-6, 100e-6, 100e-6, 100e-6])
+assert len(Ns) == len(amps)
+for i in range(len(Ns)):
+    N = Ns[i]
+    amp = amps[i]
+    label = labels[i]
+    I_min = I_mins[i]
+    dV = dVs[i]
+    process_I_H(os.path.join(data_dir + data_prefix + N + data_format), amp=amp, I_min=I_min, dV=dV, ax=ax, label=label)
+ax.legend(loc='best')
+fig.savefig(os.path.join(figs_dir + 'ZBC_field_cooling_1T' + fig_format))
+
+
+#%%
+fig = plt.figure()
+ax = plt.subplot(111)
+process_record(os.path.join(data_dir + data_prefix + '0040' + data_format), amp=1e9, I_min=0, dV=100e-6, ax=ax, label='')
+fig.savefig(os.path.join(figs_dir + 'record_40' + fig_format))
+
+
+#%%
+fig = plt.figure()
+ax = plt.subplot(111)
+process_record(os.path.join(data_dir + data_prefix + '0111' + data_format), amp=1e10, I_min=0, dV=100e-6, ax=ax, label='')
+fig.savefig(os.path.join(figs_dir + 'record_111' + fig_format))
+ax.set_xlim(0, 300)
+fig.savefig(os.path.join(figs_dir + 'record_111_zoom_300' + fig_format))
+ax.set_xlim(0, 50)
+fig.savefig(os.path.join(figs_dir + 'record_111_zoom_50' + fig_format))
+
+#%%
+sweeped, measured, data, measured_time, [T_min, T_max] = read_mesdata(os.path.join(data_dir + data_prefix + '0111' + data_format))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+_ = ax.plot(data[::2,2], '.')
+ax.set_xlim([0, ])
+
+#%%
 plt.show()
 
 if False:
